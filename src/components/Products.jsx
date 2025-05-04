c4751b5576ac3e181b55ebe5df99729c762a3643
Sarankumar M <123160858+Sarankumar2622@users.noreply.github.com>
2025-05-04 17:02:12 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -12,7 +12,8 @@ const Products = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState(data);
   const [loading, setLoading] = useState(false);
-const [componentMounted, setComponentMounted] = useState(true);
+const componentMounted = useRef(true);
+
 
   const dispatch = useDispatch();
 
@@ -24,15 +25,15 @@ const [componentMounted, setComponentMounted] = useState(true);
     const getProducts = async () => {
       setLoading(true);
       const response = await fetch("https://fakestoreapi.com/products/");
-      if (componentMounted) {
+      if (componentMounted.current) {
         setData(await response.clone().json());
         setFilter(await response.json());
         setLoading(false);
       }
 
-      return () => {
-        setComponentMounted(false);
-      };
+       return () => {
+    componentMounted.current = false;
+  };
     };
 
     getProducts();
